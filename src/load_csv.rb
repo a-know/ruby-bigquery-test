e77115ec5548f206b82448324b0c50c7e989427d
a-know <a.know.3373@gmail.com>
2014-04-27 18:37:36 +0900

    Initialize the client with authentication.

--- src/load_csv.rb
+++ src/load_csv.rb
@@ -1,17 +1,21 @@
 require 'bundler/setup'
 require 'google/api_client'
-require 'google/api_client/client_secrets'
+require 'yaml'
+
+# load credential yaml
+oauth_yaml = YAML.load_file('.google-api.yaml')
 
 # Initialize the client.
 client = Google::APIClient.new(
   :application_name => 'Example Ruby Bigquery',
-  :application_version => '1.0.0'
-)
+  :application_version => '1.0.0')
+client.authorization.client_id = oauth_yaml["client_id"]
+client.authorization.client_secret = oauth_yaml["client_secret"]
+client.authorization.scope = oauth_yaml["scope"]
+client.authorization.refresh_token = oauth_yaml["refresh_token"]
+client.authorization.access_token = oauth_yaml["access_token"]
 
 # Initialize Bigquery client.
 bq_client = client.discovered_api('bigquery', 'v2')
-
-# Load client secrets from your client_secrets.json.
-client_secrets = Google::APIClient::ClientSecrets.load
 
 p bq_client
