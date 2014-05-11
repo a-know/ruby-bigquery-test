fe28c6eef06d2054b8473fa906891215e5a1e8f0
a-know <a.know.3373@gmail.com>
2014-05-11 11:17:12 +0900

    change authentication to use client_secrets.json

--- src/get_access_token.rb
+++ src/get_access_token.rb
@@ -11,12 +11,13 @@ client = Google::APIClient.new(
  
 # Run installed application flow. Check the samples for a more
 # complete example that saves the credentials between runs.
-credential = File.read('client_secrets.txt').split(',')
+# credential = File.read('client_secrets.txt').split(',')
+credential = Google::APIClient::ClientSecrets.load
  
  
 flow = Google::APIClient::InstalledAppFlow.new(
-  :client_id => credential[0],
-  :client_secret => credential[1],
+  :client_id => credential.client_id,
+  :client_secret => credential.client_secret,
   :scope => ['https://www.googleapis.com/auth/bigquery']
 )
 client.authorization = flow.authorize
