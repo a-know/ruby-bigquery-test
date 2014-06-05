5c19362593d0623f8ed008dbaff6d3ef0c0133b3
a-know <a.know.3373@gmail.com>
2014-06-05 20:52:13 +0900

    Upload file to Google Cloud Storage by fog

---
+++ src/upload_file.rb
@@ -0,0 +1,34 @@
+require 'bundler/setup'
+require 'rubygems'
+require 'fog'
+require 'yaml'
+
+# load credential yaml
+credential_yaml = YAML.load_file('.gcs-credential.yaml')
+
+# create a connection
+connection = Fog::Storage.new({
+  :provider                         => 'Google',
+  :google_storage_access_key_id     => oauth_yaml["google_storage_access_key_id"],
+  :google_storage_secret_access_key => oauth_yaml["google_storage_secret_access_key"],
+})
+
+# First, a place to contain the glorious details
+directory = connection.directories.create(
+  :key    => "fog-demo-#{Time.now.to_i}", # globally unique name
+)
+
+# list directories
+p connection.directories
+
+# upload that resume
+file = directory.files.create(
+  :key    => 'resume.html',
+  :body   => File.open("/path/to/my/resume.html"),
+)
+
+# # あるバケットの中を参照
+# directory = connection.directories.get("a-know-df-test")
+
+# p directory
+# p directory.files
